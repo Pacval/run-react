2ffd28ed7d70c5f924c27a6fb6758e7b7507dd2d
Valentin ROCHET <valentin.rochet@plebicom.com>
2019-12-09 16:40:27 +0100

    Add images

--- src/components/GameCase/index.js
+++ src/components/GameCase/index.js
@@ -1,14 +1,35 @@
 import React from "react";
 
 import style from "./gamecase.module.css";
-import { PLAYER } from "../../constants/gameCaseTypes";
+import { PLAYER, OBSTACLE, ENEMY, EXIT } from "../../constants/gameCaseTypes";
 
 export default ({ type, torch }) => {
   return (
     <div className={style.caseMainDiv}>
-      {type === PLAYER && (
-        <img className={style.image} src={require("./img/player.png")} alt="" />
-      )}
+      {(type === OBSTACLE && (
+        <img
+          className={style.image}
+          src={require("./img/obstacle.png")}
+          alt=""
+        />
+      )) ||
+        (type === ENEMY && (
+          <img
+            className={style.image}
+            src={require("./img/enemy.png")}
+            alt=""
+          />
+        )) ||
+        (type === PLAYER && (
+          <img
+            className={style.image}
+            src={require("./img/player.png")}
+            alt=""
+          />
+        )) ||
+        (type === EXIT && (
+          <img className={style.image} src={require("./img/exit.png")} alt="" />
+        ))}
     </div>
   );
 };
