1c4664599dfe908d10d0ea3fc7f0a60818d23a75
Valentin ROCHET <val.rochet@gmail.com>
2020-01-10 09:41:05 +0100

    log in and sign up

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -6,43 +6,82 @@ import useUser from "../../utils/useUser";
 export default () => {
   const { user, authenticate } = useUser();
 
-  const [authPseudo, setAuthPseudo] = useState("");
-  const [authPassword, setAuthPassword] = useState("");
+  const [modeLogin, setModeLogin] = useState(true);
 
-  const handleClickAuthent = () => {
+  const [logInUsername, setLogInUsername] = useState("");
+  const [logInPassword, setLogInPassword] = useState("");
+
+  const [signUpUsername, setSignUpUsername] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+
+  const handleClickLogIn = () => {
     authenticate({
-      username: authPseudo,
-      password: authPassword
+      username: logInUsername,
+      password: logInPassword
     });
   };
+
+  const handleClickSignUp = () => {};
 
   return (
     <>
       <Link to="/">Revenir au menu</Link>
       {!user ? (
-        <>
-          <label>
-            Pseudo :
-            <input
-              value={authPseudo}
-              onChange={event => setAuthPseudo(event.target.value)}
-            ></input>
-          </label>
-          <label>
-            Mot de passe :
-            <input
-              type="password"
-              value={authPassword}
-              onChange={event => setAuthPassword(event.target.value)}
-            ></input>
-          </label>
-          <button
-            onClick={handleClickAuthent}
-            disabled={authPseudo === "" || authPassword === ""}
-          >
-            Connexion
-          </button>
-        </>
+        modeLogin ? (
+          <>
+            {/* log in */}
+            <label>
+              Username :
+              <input
+                value={logInUsername}
+                onChange={event => setLogInUsername(event.target.value)}
+              ></input>
+            </label>
+            <label>
+              Mot de passe :
+              <input
+                type="password"
+                value={logInPassword}
+                onChange={event => setLogInPassword(event.target.value)}
+              ></input>
+            </label>
+            <button
+              onClick={handleClickLogIn}
+              disabled={logInUsername === "" || logInPassword === ""}
+            >
+              Connexion
+            </button>
+            Pas de compte ?
+            <button onClick={() => setModeLogin(false)}>Créer un compte</button>
+          </>
+        ) : (
+          <>
+            {/* sign up */}
+            <label>
+              Username :
+              <input
+                value={signUpUsername}
+                onChange={event => setSignUpUsername(event.target.value)}
+              ></input>
+            </label>
+            <label>
+              Mot de passe :
+              <input
+                type="password"
+                value={signUpPassword}
+                onChange={event => setSignUpPassword(event.target.value)}
+              ></input>
+            </label>
+            <button
+              onClick={handleClickSignUp}
+              disabled={signUpUsername === "" || signUpPassword === ""}
+            >
+              Créer
+            </button>
+            Déjà un compte ?
+            <button onClick={() => setModeLogin(true)}>Créer un compte</button>
+          </>
+        )
       ) : (
         <>{user.pseudo}</>
       )}
